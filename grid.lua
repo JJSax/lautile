ce4af7b59d6b2fbf00f0c6cf3975ef36a4219cc4
JJSax <5084938+JJSax@users.noreply.github.com>
2024-04-11 21:22:42 -0600

    Add isValidCell and adjacent/iteration funcs

--- grid.lua
+++ grid.lua
@@ -2,7 +2,8 @@
 
 local Grid = {}
 Grid.__index = Grid
-Grid._VERSION = "2.0.0"
+Grid._VERSION = "2.0.3"
+
 
 --[[
 Tiles will have x and y to be it's position in the Grid
@@ -34,23 +35,13 @@ local function interpretDefault(d)
 	elseif type(d) == "function" then
 		return d
 	elseif type(d) == "nil" then
-		return function() end
+		return function() return {} end
 	else
 		error("type of tile can only be of type table or function.", 3)
 	end
 end
 
-local function call(self, x, y)
-
-end
-
--- function Grid.__index(self, i)
-
--- end
 Grid.__index = Grid
--- Grid.__newindex = function(self, k, v)
--- 	print(self, k, v)
--- end
 
 function Grid.__call(self, x, y)
 	expect(x, "number", "x")
@@ -62,7 +53,7 @@ function Grid.__call(self, x, y)
 
 	assert(not self.strict, "Grid is strict; You cannot index cells that don't exist.")
 
-	local t = self.defaultTile()
+	local t = self.defaultTile(self, x, y)
 	t.x, t.y = x, y
 	if not self.tiles[x] then self.tiles[x] = {} end
 	self.tiles[x][y] = t
@@ -72,7 +63,7 @@ end
 function Grid.new(tile, width, height, strict) -- strict disallows indexing cells that are oustide w/h
 	local self = setmetatable({
 		tiles = {},
-		defaultTile = interpretDefault(tile)
+		defaultTile = interpretDefault(tile),
 	}, Grid)
 
 	if not width then return self end
@@ -89,23 +80,51 @@ function Grid.new(tile, width, height, strict) -- strict disallows indexing cell
 	return self
 end
 
-function Grid:setTile(x, y, data)
-	--? consider new file to be used in this file
-	--? that allows unordered indices still have a single size integer
-	if not data then
-		assert(self.tiles[x], "Requires x position to be valid.")
-		self.tiles[x][y] = nil
-		return
-	end
-	self.tiles[x] = self.tiles[x] or {}
-	self.tiles[x][y] = data
+function Grid:isValidCell(x, y)
+	if type(x) == "table" then return x.grid == self end
+	return self.tiles[x] and self.tiles[x][y]
 end
 
-function Grid:getTile(x, y)
-	if not self.tiles[x] then
-		return {}
+function Grid:getAdjacent(x, y, diagonals)
+	-- loop through adjacent cells to x, y
+	-- if diagonals is true, add diagonals
+
+	local adjacent = {}
+	for lx = -1, 1 do
+		for ly = -1, 1 do
+			if lx == 0 and ly == 0 then -- do nothing
+			elseif self:isValidCell(x + lx, y + ly) and
+			((diagonals and lx ~= 0 and ly ~= 0) or (lx==0 or ly==0)) then
+				table.insert(adjacent, self.tiles[x + lx][y + ly])
+			end
+		end
 	end
-	return self.tiles[x][y] or {}
+	return adjacent
+end
+
+function Grid:iterate()
+	return coroutine.wrap(function()
+		for _, xt in pairs(self.tiles) do
+			for _, tile in pairs(xt) do
+				coroutine.yield(tile, tile.x, tile.y)
+			end
+		end
+	end)
+end
+
+function Grid:iterateAdjacent(x, y, diagonals)
+	local adjacent = self:getAdjacent(x, y, diagonals)
+	local i = 0
+	return function()
+		i = i + 1
+		if i <= #adjacent then
+			return adjacent[i], adjacent[i].x, adjacent[i].y
+		end
+	end
+end
+
+function Grid:getRandomCell()
+	return
 end
 
 return Grid
