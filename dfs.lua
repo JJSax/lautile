8b6ec96696ab95eb9158964be4033849802683be
JJSax <5084938+JJSax@users.noreply.github.com>
2024-05-20 17:41:36 -0600

    Fix premature finish.

--- dfs.lua
+++ dfs.lua
@@ -3,25 +3,25 @@ local Pathfinding = require(HERE..".pathfinding")
 
 local dfs = setmetatable({}, { __index = Pathfinding })
 dfs.__index = dfs
-dfs._VERSION = "0.0.2"
+dfs._VERSION = "0.0.3"
 
 function dfs.new(grid, startTile, target)
 	return setmetatable(Pathfinding.new(grid, startTile, target), dfs)
 end
 
 local function __NULL__(...) end
-for k,v in ipairs({"exploreTile", "markDeadEnd"}) do
+for _,v in ipairs({"exploreTile", "markDeadEnd"}) do
 	dfs[v] = __NULL__
 end
 
 function dfs:step()
 	if #self.stack > 0 and not self.complete then
 		local currentCell = table.remove(self.stack)
+		self.currentTile = currentCell -- the tile object
 		if self:isTarget(currentCell) then
 			self.complete = true
 			return self.visited
 		end
-		self.currentTile = currentCell -- the tile object
 		local neighbors = self:getUnvisitedNeighbors(currentCell)
 		if #neighbors > 0 then
 			table.insert(self.stack, currentCell)
