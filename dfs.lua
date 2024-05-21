f3ca1f2d4a2be60f78b2932dce3394c98ba40de7
JJSax <5084938+JJSax@users.noreply.github.com>
2024-05-21 11:41:12 -0600

    add dfs complete path

--- dfs.lua
+++ dfs.lua
@@ -3,11 +3,12 @@ local Pathfinder = require(HERE..".pathfinder")
 
 local dfs = setmetatable({}, { __index = Pathfinder })
 dfs.__index = dfs
-dfs._VERSION = "0.0.4"
+dfs._VERSION = "0.0.5"
 
 function dfs.new(grid, startTile, target)
 	local self = setmetatable(Pathfinder.new(grid, startTile, target), dfs)
 	self.stack = {startTile}
+	self.path = {}
 	return self
 end
 
@@ -27,11 +28,13 @@ function dfs:step()
 		end
 		local neighbors = self:getUnvisitedNeighbors(currentCell)
 		if #neighbors > 0 then
+			table.insert(self.path, currentCell)
 			table.insert(self.stack, currentCell)
 			local nextCell = neighbors[math.random(1, #neighbors)]
 			self.visited[nextCell] = true
 			table.insert(self.stack, nextCell)
 		else
+			table.remove(self.path)
 			self:markDeadEnd(currentCell)
 		end
 	else
